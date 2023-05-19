7790a008c12c9ea697db3d9cd93989c50a8d5a85
claramcms <127965051+claramcms@users.noreply.github.com>
2023-05-19 11:49:32 -0300

    Primeira aula sobre python

---
+++ pyton/aula.py
@@ -0,0 +1,20 @@
+print("Alguma coisa")
+
+print('Alguma "coisa"')
+
+print("Alguma \"coisa\"")
+
+print("Alguma \n coisa")
+
+print(1+1)
+
+print("1 x 1 =", 1*1)
+
+print(10/2)
+
+print("10 / 2 =", 10/2)
+
+print("10² =", 10**2)
+
+print("10² =", pow(10,2))
+
